a6bc887b8d729dafc0adf1d55bdbcad20b2507a8
iliewrado <87384771+iliewrado@users.noreply.github.com>
2022-02-11 23:14:34 +0200

    Heaps and Binary Trees - Lab

---
+++ 01. Fundamentals/05. Heaps and Binary Trees - Lab/01.BinaryTree/BinaryTree.cs
@@ -0,0 +1,128 @@
+﻿namespace _01.BinaryTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class BinaryTree<T> : IAbstractBinaryTree<T>
+    {
+        public BinaryTree(T value
+            , IAbstractBinaryTree<T> leftChild
+            , IAbstractBinaryTree<T> rightChild)
+        {
+            this.Value = value;
+            this.LeftChild = leftChild;
+            this.RightChild = rightChild;
+        }
+
+        public T Value { get; private set; }
+
+        public IAbstractBinaryTree<T> LeftChild { get; private set; }
+
+        public IAbstractBinaryTree<T> RightChild { get; private set; }
+
+        public string AsIndentedPreOrder(int indent)
+        {
+            return PreorderDfs(indent, this);
+        }
+
+        public List<IAbstractBinaryTree<T>> InOrder()
+        {
+            List<IAbstractBinaryTree<T>> result = new List<IAbstractBinaryTree<T>>();
+            DfsInOrder(this, result);
+            return result;
+        }
+
+        public List<IAbstractBinaryTree<T>> PostOrder()
+        {
+            List<IAbstractBinaryTree<T>> result = new List<IAbstractBinaryTree<T>>();
+            DfsPostOrder(this, result);
+            return result;
+        }
+
+        public List<IAbstractBinaryTree<T>> PreOrder()
+        {
+            List<IAbstractBinaryTree<T>> result = new List<IAbstractBinaryTree<T>>();
+            DfsPreOrder(this, result);
+            return result;
+        }
+
+        public void ForEachInOrder(Action<T> action)
+        {
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.ForEachInOrder(action);
+            }
+
+            action.Invoke(this.Value);
+
+            if (this.RightChild != null)
+            {
+                this.RightChild.ForEachInOrder(action);
+            }
+        }
+
+        private void DfsPreOrder(IAbstractBinaryTree<T> tree, List<IAbstractBinaryTree<T>> result)
+        {
+            result.Add(tree);
+            
+            if (tree.LeftChild != null)
+            {
+                DfsPreOrder(tree.LeftChild, result);
+            }
+
+            if (tree.RightChild != null)
+            {
+                DfsPreOrder(tree.RightChild, result);
+            }
+        }
+
+        private void DfsPostOrder(IAbstractBinaryTree<T> tree, List<IAbstractBinaryTree<T>> result)
+        {
+            if (tree.LeftChild != null)
+            {
+                DfsPostOrder(tree.LeftChild, result);
+            }
+
+            if (tree.RightChild != null)
+            {
+                DfsPostOrder(tree.RightChild, result);
+            }
+            
+            result.Add(tree);
+        }
+
+        private void DfsInOrder(IAbstractBinaryTree<T> tree, List<IAbstractBinaryTree<T>> result)
+        {
+            if (tree.LeftChild != null)
+            {
+                DfsInOrder(tree.LeftChild, result);
+            }
+            
+            result.Add(tree);
+
+            if (tree.RightChild != null)
+            {
+                DfsInOrder(tree.RightChild, result);
+            }
+        }
+
+        private string PreorderDfs(int spaceLevel, IAbstractBinaryTree<T> node)
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append(new string(' ', spaceLevel));
+            result.AppendLine(node.Value.ToString());
+
+            if (node.LeftChild != null)
+            {
+                result.AppendLine(PreorderDfs(spaceLevel + 2, node.LeftChild));
+            }
+            if (node.RightChild != null)
+            {
+                result.AppendLine(PreorderDfs(spaceLevel + 2, node.RightChild));
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 01. Fundamentals/05. Heaps and Binary Trees - Lab/02.MaxHeap/MaxHeap.cs
@@ -0,0 +1,49 @@
+﻿namespace _02.MaxHeap
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MaxHeap<T> : IAbstractHeap<T>
+        where T : IComparable<T>
+    {
+        private List<T> heap;
+
+        public MaxHeap()
+        {
+            this.heap = new List<T>();
+        }
+
+        public int Size => heap.Count;
+
+        public void Add(T element)
+        {
+            heap.Add(element);
+            HeapifyUp(heap.Count - 1);
+        }
+                
+        public T Peek()
+        {
+            if (heap.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return heap[0];
+        }
+
+        private void HeapifyUp(int index)
+        {
+            if (index == 0) return;
+
+            int parentIndex = (index - 1) / 2;
+
+            if (heap[index].CompareTo(heap[parentIndex]) > 0)
+            {
+                T temp = heap[index];
+                heap[index] = heap[parentIndex];
+                heap[parentIndex] = temp;
+                HeapifyUp(parentIndex);
+            }
+        }
+    }
+}
