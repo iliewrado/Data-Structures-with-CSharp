e8e3917cb5f41bb4e7359684503c6477dfafe9fa
iliewrado <87384771+iliewrado@users.noreply.github.com>
2022-03-18 22:30:25 +0200

    Linear Data Structures Lab

---
+++ 01. Fundamentals/01. Linear Data Structures - Lab/Problem01.List/List.cs
@@ -0,0 +1,142 @@
+﻿namespace Problem01.List
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class List<T> : IAbstractList<T>
+    {
+        private const int DEFAULT_CAPACITY = 4;
+        private T[] _items;
+
+        public List()
+            : this(DEFAULT_CAPACITY)
+        {
+            _items = new T[DEFAULT_CAPACITY];
+        }
+
+        public List(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+
+            _items = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                this.ValidateIndex(index);
+                return this._items[index];
+            }
+            set
+            {
+                this.ValidateIndex(index);
+                this._items[index] = value;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            this.GrowIfNecessery();
+            this._items[Count++] = item;
+        }
+
+        public bool Contains(T item)
+        {
+            return this._items.Contains(item); 
+        }
+
+
+        public int IndexOf(T item)
+        {
+            if (this.Contains(item))
+            {
+                for (int i = 0; i < Count; i++)
+                {
+                    if (_items[i].Equals(item))
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            this.ValidateIndex(index);
+            this.GrowIfNecessery();
+
+            for (int i = Count; i > index; i--)
+            {
+                this._items[i] = this._items[i - 1];
+            }
+
+            this._items[index] = item;
+            this.Count++;
+        }
+
+        public bool Remove(T item)
+        {
+            if (this.Contains(item))
+            {
+                int index = this.IndexOf(item);
+                this.RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+            
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this._items[i] = this._items[i + 1];
+            }
+
+            this._items[Count - 1] = default;
+            this.Count--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this._items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => this.GetEnumerator();
+
+        private void GrowIfNecessery()
+        {
+            if (this.Count == this._items.Length)
+            {
+                this._items = this.Grow();
+            }
+        }
+
+        private T[] Grow()
+        {
+            T[] newArray = new T[this.Count * 2];
+            Array.Copy(_items, newArray, _items.Length);
+            return newArray;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+                throw new IndexOutOfRangeException(nameof(index));
+        }
+    }
+}
