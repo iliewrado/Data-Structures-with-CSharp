e4deb8121ac967582f1557eb84ffddc3b70ee157
iliewrado <87384771+iliewrado@users.noreply.github.com>
2022-04-27 13:51:34 +0300

    04. Exercise AVL Trees and AA-Trees

---
+++ 02. Advanced/04. Exercise AVL Trees and AA-Trees/AVLTree/AVL.cs
@@ -0,0 +1,215 @@
+﻿namespace AVLTree
+{
+    using System;
+
+    public class AVL<T> where T : IComparable<T>
+    {
+        public Node<T> Root { get; private set; }
+
+        public bool Contains(T item)
+        {
+            var node = this.Search(this.Root, item);
+            return node != null;
+        }
+
+        public void Insert(T item)
+        {
+            this.Root = this.Insert(this.Root, item);
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            this.EachInOrder(this.Root, action);
+        }
+
+        private Node<T> Insert(Node<T> node, T item)
+        {
+            if (node == null)
+            {
+                return new Node<T>(item);
+            }
+
+            int cmp = item.CompareTo(node.Value);
+            if (cmp < 0)
+            {
+                node.Left = this.Insert(node.Left, item);
+            }
+            else if (cmp > 0)
+            {
+                node.Right = this.Insert(node.Right, item);
+            }
+
+            node = this.Balance(node);
+            this.UpdateHeight(node);
+
+            return node;
+        }
+
+        private void UpdateHeight(Node<T> node)
+        {
+            node.Height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
+        }
+
+        private Node<T> Balance(Node<T> node)
+        {
+            int balanceFactor = this.Height(node.Left)
+                - this.Height(node.Right);
+
+            if (balanceFactor > 1)
+            {
+                int balance = this.Height(node.Left.Left)
+                    - this.Height(node.Left.Right);
+
+                if (balance < 0)
+                {
+                    node.Left = this.RotateLeft(node.Left);
+                }
+
+                node = this.RotateRight(node);
+            }
+            else if (balanceFactor < -1)
+            {
+                int balance = this.Height(node.Right.Left)
+                    - this.Height(node.Right.Right);
+
+                if (balance > 0)
+                {
+                    node.Right = this.RotateRight(node.Right);
+                }
+
+                node = this.RotateLeft(node);
+            }
+
+            return node;
+        }
+
+        private Node<T> RotateLeft(Node<T> node)
+        {
+            Node<T> right = node.Right;
+            node.Right = right.Left;
+            right.Left = node;
+
+            this.UpdateHeight(node);
+
+            return right;
+        }
+
+        private Node<T> RotateRight(Node<T> node)
+        {
+            Node<T> left = node.Left;
+            node.Left = left.Right;
+            left.Right = node;
+
+            this.UpdateHeight(node);
+
+            return left;
+        }
+
+        private Node<T> Search(Node<T> node, T item)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            int cmp = item.CompareTo(node.Value);
+            if (cmp < 0)
+            {
+                return Search(node.Left, item);
+            }
+            else if (cmp > 0)
+            {
+                return Search(node.Right, item);
+            }
+
+            return node;
+        }
+
+        private void EachInOrder(Node<T> node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.EachInOrder(node.Left, action);
+            action(node.Value);
+            this.EachInOrder(node.Right, action);
+        }
+
+        private int Height(Node<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return node.Height;
+        }
+
+        public void Delete(T element)
+        {
+            this.Root = this.Delete(this.Root, element);
+        }
+
+        private Node<T> Delete(Node<T> node, T element)
+        {
+            if (node == null)
+                return null;
+            
+            int cmp = element.CompareTo(node.Value);
+            if (cmp < 0)
+            {
+                node.Left = this.Delete(node.Left, element);
+            }
+            else if (cmp > 0)
+            {
+                node.Right = this.Delete(node.Right, element);
+            }
+            else
+            {
+                if (node.Left == null && node.Right == null)
+                {
+                    return null;
+                }
+                else if (node.Left == null)
+                {
+                    node = node.Right;
+                }
+                else if (node.Right == null)
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    Node<T> temp = this.FindMin(node.Right);
+                    node.Value = temp.Value;
+
+                    node.Right = this.Delete(node.Right, temp.Value);
+                }
+            }
+
+            node = this.Balance(node);
+            this.UpdateHeight(node);
+
+            return node;
+        }
+
+        private Node<T> FindMin(Node<T> node)
+        {
+            if (node.Left == null)
+                return node;
+
+            return this.FindMin(node.Left);
+        }
+
+        public void DeleteMin()
+        {
+            if (this.Root == null)
+                return;
+
+            Node<T> min = this.FindMin(this.Root);
+            this.Root = this.Delete(this.Root, min.Value);
+        }
+    }
+}
